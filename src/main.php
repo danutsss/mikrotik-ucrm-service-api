30e76a388766825fb54190a13c21e16e0fd24a62
Matei Nicolae - Daniel <matei1337@icloud.com>
2021-11-16 16:44:47 +0200

    Added ip_in_range function.

--- src/main.php
+++ src/main.php
@@ -1,3 +1,20 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+chdir(__DIR__);
+
+require 'vendor/autoload.php';
+
+(static function() {
+    $pluginBuilder = new \DI\ContainerBuilder();
+    $pluginBuilder -> setDefinitionCache(new \Doctrine\Common\Cache\ApcuCache());
+    $pluginContainer = $pluginBuilder -> build();
+    $plugin = $pluginContainer -> get(\MikrotikService\Plugin::class);
+
+    try {
+        $plugin -> run();
+    } catch(Exception $e) {
+        $pluginLogger = new \MikrotikService\Service\Logger();
+        $pluginLogger -> error($e -> getMessage());
+        $pluginLogger -> debug($e -> getTraceAsString());
+    }
+})();
--- src/public.php
+++ src/public.php
@@ -1,30 +1,3 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-use Ubnt\UcrmPluginSdk\Service\PluginLogManager;
-
-$API = new RouterosAPI();
-
-$API -> debug = true;
-
-if($API -> connect('93.119.183.66', 'admin', 'stf@07internet')) {
-    $API -> comm("/ppp/secret/add", array(
-        "name" => "user-test",
-        "password" => "password-test",
-        "remote-address" => "192.33.54.221",
-        "comment" => "str. x, nr. y {test-comment}",
-        "service" => "pppoe",
-    ));
-    $API -> comm("/ppp/active-connections/add", array(
-        "name" => "user-test",
-        "service" => "pppoe",
-        "address" => "192.33.54.221",
-        "comment" => "str. x, nr. y {test-comment}",
-    ));
-
-    $logger = PluginLogManager::create();
-    $logger -> appendLog('PPP Instance created successfully.');
-
-    $API -> disconnect();
-}
+require_once 'main.php';
